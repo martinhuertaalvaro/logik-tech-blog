48be61967dc18678f902dcf7eb6b8f5aaf6cd3c5
Álvaro Martín Huerta <amartinh@hiberus.com>
2024-11-11 16:47:57 +0100

    changes

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
 	site_name: 'logik.dev',
-	title: 'logik.dev',
-	description: 'Blog built with Astro with research and educational finds',
+	title: 'logik.dev - blog',
+	description: 'Crafting the Digital Future with Software Development Wonders',
 	author: 'Álvaro Martín Huerta',
 	locale_region: 'es',
 	locale: 'es'
